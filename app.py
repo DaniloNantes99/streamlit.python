0fc2d2426a1e5ca6323b678b1aa4ad72d838d540
DaniloNantes99 <nantesd99@gmail.com>
2025-02-07 09:40:56 -0300

    Update app.py
    image change

--- app.py
+++ app.py
@@ -7,15 +7,14 @@ def buscar_letra(banda, musica):
     letra = response.json()["lyrics"] if response.status_code == 200 else "" 
     return letra
 
-
 def salvar_sugestao(sugestao):
     with open("sugestoes.txt", "a") as f:
         f.write(sugestao + "\n")
 
     
-st.image("robo_dancante.webp", caption = "Robo ouvindo musica", use_column_width=True)
-st.title("A letra da sua musica!")
+st.image("robo_dancante.webp", caption = "robo alegre", use_column_width=True)
 
+st.title("Letras de musicas")
 
 st.info("Desenvolvido por Danilo Nantes")
 
